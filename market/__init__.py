c61913e3b7b85486e79c077d8210b4f54e07cbe3
danulawa <danula.wa@gmail.com>
2022-12-13 11:10:54 +0530

    final push work fail

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from market.models import User, Item
+from market import routes
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
@@ -11,4 +15,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
-from market import routes
+
+with app.app_context():
+    db.create_all()
